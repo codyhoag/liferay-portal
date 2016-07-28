c53fc32e1fa7128c328f9ec1d4c046066ca4e8b7
Kevin Tan <kevin.tan@liferay.com>
2016-07-27 18:13:01 -0700

    LPS-71460 Search Bar Portlet
    https://issues.liferay.com/browse/LPS-71460

---
+++ modules/apps/foundation/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/searchbar/constants/SearchBarConstants.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.web.internal.searchbar.constants;
+
+/**
+ * @author André de Oliveira
+*/
+public class SearchBarConstants {
+
+	public static final String CONFIGURATION_JSP_PATH =
+		"/search_bar/configuration.jsp";
+
+	public static final String CSS_CLASS_WRAPPER = "portlet-search-bar";
+
+	public static final String DISPLAY_NAME = "Search Bar";
+
+	public static final String HEADER_PORTLET_CSS = "/search_bar/css/main.css";
+
+	public static final String VIEW_TEMPLATE = "/search_bar/view.jsp";
+
+}
